7a62478e98d1a2d8b37f23fd1c981d5f417ba8f3
jellevhb <jelle.vanhaeverbeke@outlook.be>
2020-02-18 13:45:56 +0100

    Use a promise instead

--- assets/js/filter-projects.js
+++ assets/js/filter-projects.js
@@ -1,9 +1,12 @@
 $('body').on('click', '.project-tag', function(){
     var theme = $(this).data('showtag');
 
-    $(".thumb-unit").fadeOut("fast", function(){
-        $('.thumb-unit.'+theme).fadeIn("fast");
-    });
+    $(".thumb-unit")
+        .fadeOut("fast")
+        .promise()
+        .done(function(){
+            $('.thumb-unit.'+theme).fadeIn("fast");
+        });
 });
 
 $('body').on('click', '.show-all-projects', function(){
